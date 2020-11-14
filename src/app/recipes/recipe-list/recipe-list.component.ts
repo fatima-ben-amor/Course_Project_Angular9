d141ae9257e9cf27f6af31d34cb1cf4ed6cf4bbd
Fatima BEN AMOR <fatima.benamor96@gmail.com>
2020-11-14 15:21:55 +0100

    Using a service for Cross-Component Communication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -9,7 +9,6 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   //new event
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes : Recipe[]
   constructor( private recipeService: RecipeService) {
     this.recipes = this.recipeService.getRecipe();
@@ -17,7 +16,5 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onRecipeSelected(recipe : Recipe){
-  this.recipeWasSelected.emit(recipe);
-}
+
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
 import { from } from 'rxjs';
 import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
+
 export class RecipeService{
+     recipeSelected = new EventEmitter<Recipe>();
+
     private recipes : Recipe[] =[
         new Recipe('A test Recipe', 'this is our first test', 'https://upload.wikimedia.org/wikipedia/commons/2/2c/GNOME_Recipes_logo.png'),
         new Recipe('A second Recipe', 'this is our the second test', 'https://cdn2.tompress.be/I-Grande-33761-livre-de-recettes-pour-potager-veritable-volume-1.net.jpg')
